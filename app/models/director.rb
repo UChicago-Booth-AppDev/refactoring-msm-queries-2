732c449393a0d1b51237a3ba9127749ccd7cc900
Guilherme Grisi <ggrisi@chicagobooth.edu>
2023-05-04 16:21:02 +0000

    Refactoring MSM queries really the final version

--- app/models/director.rb
+++ app/models/director.rb
@@ -12,7 +12,17 @@
 #
 class Director < ApplicationRecord
   
+  # DIRECTOR TO MOVIE
+
+  # One director can write many movies, but a movie has only one director (usually)
+
+  # Therefore, this is a 1-to-many relationship, and we have to use the has_many() function.
+
+  # Since the method's name (filmography) is not the same as the table's name (movie), we can't use the abbreviated version of the function.
+
   has_many(:filmography, { :foreign_key => "director_id", :class_name => "Movie"})
+
+  # This code does the same as this code below:
   
   #def filmography
    # key = self.id
